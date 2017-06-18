976dd3c3817836c6866d2c2df24e8e4d9c86ff69
Michael Nomitch <mikenomitch@gmail.com>
2017-06-18 00:57:33 -0500

    adding heroku config

--- config/prod.exs
+++ config/prod.exs
@@ -15,21 +15,28 @@ config :danton, Danton.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [host: "example.com", port: 80],
   cache_static_manifest: "priv/static/manifest.json"
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Do not print debug messages in production
 config :logger, level: :info
+
+config :danton, Danton.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true
 
 # ## SSL Support
 #
 # To get SSL working, you will need to add the `https` key
 # to the previous section and set your `:url` port to 443:
 #
-#     config :danton, Danton.Endpoint,
-#       ...
-#       url: [host: "example.com", port: 443],
-#       https: [port: 443,
-#               keyfile: System.get_env("SOME_APP_SSL_KEY_PATH"),
-#               certfile: System.get_env("SOME_APP_SSL_CERT_PATH")]
+config :danton, Danton.Endpoint,
+  ...
+  url: [scheme: "https", host: "stormy-reef-53700.herokuapp.com", port: 443], force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  https: [port: 443,
+          keyfile: System.get_env("SOME_APP_SSL_KEY_PATH"),
+          certfile: System.get_env("SOME_APP_SSL_CERT_PATH")]
 #
 # Where those two env variables return an absolute path to
 # the key and cert in disk or a relative path inside priv,
@@ -58,4 +65,5 @@ config :logger, level: :info
 
 # Finally import the config/prod.secret.exs
 # which should be versioned separately.
-import_config "prod.secret.exs"
+# import_config "prod.secret.exs"
+# (commented out as part of heroku setup)
