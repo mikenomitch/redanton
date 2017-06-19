d46cca1b07ad30637a4ffadc439d0cd9ba5f0123
Michael Nomitch <mikenomitch@gmail.com>
2017-06-19 00:15:33 -0500

    seed tweaks for the better

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -23,24 +23,24 @@ Danton.Repo.delete_all Danton.Club
 # Make Clubs (2)
 
 clubs = [
-	%{name: "Nomitch Fam", description: "link share for the family"},
-	%{name: "Brain Food", description: "let's share some interesting stuff"}
+	%Danton.Club{name: "Nomitch Fam", description: "link share for the family"},
+	%Danton.Club{name: "Brain Food", description: "let's share some interesting stuff"}
 ]
 
-clubs = Enum.map(clubs, fn(club) -> Danton.Club.changeset(%Danton.Club{}, club) |> Danton.Repo.insert! end)
+Enum.each(clubs, fn (club) -> Danton.Repo.insert(person) end)
 
 # Make Channels
 
 # last_club = Danton.Club |> Ecto.Query.last
 
 channels = [
-	%{name: "Articles", description: "channel for articles of interest", club_id: 1},
-	%{name: "Videos", description: "videos that might be of interest", club_id: 1},
-	%{name: "Music", description: "music that might be of interest", club_id: 1},
-	%{name: "News", description: "news stories that might be of interest", club_id: 1},
+	%Danton.Channel{name: "Articles", description: "channel for articles of interest", club_id: 1},
+	%Danton.Channel{name: "Videos", description: "videos that might be of interest", club_id: 1},
+	%Danton.Channel{name: "Music", description: "music that might be of interest", club_id: 1},
+	%Danton.Channel{name: "News", description: "news stories that might be of interest", club_id: 1},
 ]
 
-Enum.map(channels, fn(chan) -> Danton.Channel.changeset(%Danton.Channel{}, chan) |> Danton.Repo.insert! end)
+Enum.each(channels, fn (club) -> Danton.Repo.insert(person) end)
 
 # Make Posts (2 per channel)
 
