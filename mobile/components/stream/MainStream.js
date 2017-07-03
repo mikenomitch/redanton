c13b46e28410cc8a949dbcc3e8759ea3e6b8515d
Michael Nomitch <mikenomitch@gmail.com>
2017-07-03 16:20:57 -0500

    fetching actual posts on main stream

--- mobile/components/stream/MainStream.js
+++ mobile/components/stream/MainStream.js
@@ -1,24 +1,30 @@
 import React from 'react'
 import Stream from './Stream'
 
+import { get } from '../../lib/fetcher'
+
 class MainStream extends React.Component {
   static navigationOptions = {
-    title: 'Your Stream'
+  	title: 'Your Stream'
   }
 
-	postsResponse () {
-		return {"data":
-			[
-				{"title":"Early Bezoz Interview","id":1,"description":"Kind of cool. Would not have thought he'd be one of the richest people in the world."},
-			  {"title":"How to Be Miserable","id":2,"description":"Interesting take on the \"how to be happy\" arguments."},
-				{"title":"Meditations on Moloch","id":3,"description":"This is kind of weird but in my opinion really interesting"},
-				{"title":"Considerations on Cost Disease","id":4,"description":"Interesting non-partisan take on rising costs."}
-			]
-		}
-	}
+  constructor(props) {
+  	super(props)
+  	this.state = {
+  		posts: []
+  	}
+  }
+
+  componentDidMount() {
+  	get('/front').then((data) => {
+  		this.setState({
+  			posts: data["data"]
+  		})
+  	})
+  }
 
   render() {
-		const streamContent = this.postsResponse()["data"]
+		const streamContent = this.state.posts
     return <Stream navigation={this.props.navigation} content={streamContent}/>
 	}
 }
--- mobile/lib/fetcher.js
+++ mobile/lib/fetcher.js
@@ -1,5 +1,9 @@
 const base = 'https://stormy-reef-53700.herokuapp.com/api/v1'
 
+function jsonify(res) {
+  return res.json()
+}
+
 export function get(endpoint) {
-	return fetch(base + endpoint).then((response) => response.json())
+  return fetch(base + endpoint).then(jsonify)
 }
