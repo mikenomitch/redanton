56a57f6826be7dc5771ae1f660d0ad238a32b6a3
Michael Nomitch <mikenomitch@gmail.com>
2017-07-04 12:08:59 -0500

    better mobile nav

--- mobile/components/navigation/MainNav.js
+++ mobile/components/navigation/MainNav.js
@@ -1,17 +1,66 @@
-import { TabNavigator } from 'react-navigation'
+import React from 'react'
+import { StackNavigator, TabNavigator } from 'react-navigation'
 
-import StreamNavigator from './StreamNav'
-import ChannelNavigator from './ChannelNav'
-import SimpleChat from '../chat/SimpleChat'
+import NewPostScreen from '../post/NewPost'
+import MainStreamScreen from '../stream/MainStream'
+import ChannelScreen from '../channel/Channel'
+import ChannelListScreen from '../channel/ChannelList'
+import PostScreen from '../post/Post'
 
-const MainNav = TabNavigator({
-  Stream: { screen: StreamNavigator },
-  Channels: { screen: ChannelNavigator },
-  Chat: { screen: SimpleChat }
+const TabNav = TabNavigator(
+  {
+    MainStreamTab: {
+      screen: MainStreamScreen,
+      path: '/',
+      navigationOptions: {
+        title: 'My Stream',
+        tabBarLabel: 'Stream'
+      },
+    },
+    ChannelsTab: {
+      screen: ChannelListScreen,
+      path: '/channels',
+      navigationOptions: {
+        title: 'Channels'
+      },
+    },
+  },
+  {
+    tabBarPosition: 'bottom',
+    animationEnabled: false,
+    swipeEnabled: false,
+  }
+)
+
+const MainNav = StackNavigator({
+  Root: {
+    screen: TabNav,
+  },
+  Post: {
+    screen: PostScreen,
+    navigationOptions: {
+      title: 'Post'
+    }
+  },
+	Channel: {
+    screen: ChannelScreen,
+    navigationOptions: {
+      title: 'Channel'
+    }
+  },
+	NewPost: {
+    screen: NewPostScreen,
+    navigationOptions: {
+      title: 'Add Post'
+    }
+  }
+  // Profile: {
+  //   screen: MyProfileScreen,
+  //   path: '/people/:name',
+  //   navigationOptions: ({ navigation }) => {
+  //     title: `${navigation.state.params.name}'s Profile!`
+  //   }
+  // }
 })
-
-MainNav.navigationOptions = {
-  title: 'Danton',
-}
 
 export default MainNav
