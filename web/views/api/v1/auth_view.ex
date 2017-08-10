ddacac9d58bdf8c359c381c7969c705bf7d6965f
Michael Nomitch <mikenomitch@gmail.com>
2017-08-09 23:04:01 -0500

    add avatar to user output

--- web/views/api/v1/auth_view.ex
+++ web/views/api/v1/auth_view.ex
@@ -2,6 +2,6 @@ defmodule Danton.Api.V1.AuthView do
 	use Danton.Web, :view
 
 	def render("login.json", %{user: user, jwt: jwt, exp: exp}) do
-    %{user: %{id: user.id, name: user.name, email: user.email}, jwt: jwt, exp: exp}
+    %{user: %{id: user.id, name: user.name, email: user.email, avatar: user.avatar}, jwt: jwt, exp: exp}
   end
 end
