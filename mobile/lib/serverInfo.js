7f73eb0f7632b3c529c46d80df52ffe39c6c4feb
Michael Nomitch <mikenomitch@gmail.com>
2017-09-06 00:20:20 -0500

    coming soon change

--- mobile/lib/serverInfo.js
+++ mobile/lib/serverInfo.js
@@ -1,3 +1,3 @@
-// export const serverUrl = 'https://stormy-reef-53700.herokuapp.com'
-export const serverUrl = 'https://c2530427.ngrok.io'
+export const serverUrl = 'https://stormy-reef-53700.herokuapp.com'
+// export const serverUrl = 'https://c2530427.ngrok.io'
 export default { serverUrl }
