040d3b6633ecf334632f5b658a5ac9a83ae47cab
Michael Nomitch <mikenomitch@gmail.com>
2017-09-26 10:02:33 -0500

    server for desktop

--- mobile/lib/serverInfo.js
+++ mobile/lib/serverInfo.js
@@ -1,3 +1,3 @@
 // export const serverUrl = 'https://stormy-reef-53700.herokuapp.com'
-export const serverUrl = 'https://f29136c4.ngrok.io'
+export const serverUrl = 'https://cb27145f.ngrok.io'
 export default { serverUrl }
