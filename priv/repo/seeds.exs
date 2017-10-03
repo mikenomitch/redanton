e2369d6280305f71bfce3fdb1400888db5f39116
Michael Nomitch <mikenomitch@gmail.com>
2017-10-03 10:57:37 -0500

    seed tweak for prod

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -19,13 +19,12 @@ Repo.delete_all Danton.Club
 # Make Users
 # ==============
 
-# passwords are "secret"
 users = [
   %Danton.User{name: "Mike", status: "active", email: "mikenomitch@gmail.com", avatar: "https://scontent.ford1-1.fna.fbcdn.net/v/t1.0-1/p240x240/13934743_4154683422348_4358878110125305408_n.jpg?oh=b9479a030c0b081197e9b4a2e91181f7&oe=5A0CC1DB"},
-  %Danton.User{name: "Dan", status: "active", email: "danmihalov@gmail.com", avatar: "https://scontent.ford1-1.fna.fbcdn.net/v/t1.0-9/1393772_2712932459475_332693732_n.jpg?oh=c8880860965ae1e28637201cd57cdff2&oe=59FA61FB"},
-  %Danton.User{name: "Steven", status: "active", email: "stevenan7@gmail.com", avatar: "https://media.licdn.com/mpr/mpr/shrinknp_200_200/p/4/005/0b5/026/15086de.jpg"},
-  %Danton.User{name: "Zach", status: "active", email: "zfertig@gmail.com", avatar: "https://pbs.twimg.com/profile_images/556526218989027328/ERy-0VYQ.jpeg"},
-  %Danton.User{name: "Kevin", status: "active", email: "kevinzussman@gmail.com", avatar: "https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAfOAAAAJDJiYTQwM2NhLWZlNjgtNDI2NS1hYTRjLTBkMzBmNjA3MTQxNg.jpg"},
+  %Danton.User{name: "Dan", status: "active", email: "danmihalov@gmail.com", avatar: "https://scontent.ford1-1.fna.fbcdn.net/v/t1.0-9/1393772_2712932459475_332693732_n.jpg?oh=c8880860965ae1e28637201cd57cdff2&oe=59FA61FB"}
+  # %Danton.User{name: "Steven", status: "active", email: "stevenan7@gmail.com", avatar: "https://media.licdn.com/mpr/mpr/shrinknp_200_200/p/4/005/0b5/026/15086de.jpg"},
+  # %Danton.User{name: "Zach", status: "active", email: "zfertig@gmail.com", avatar: "https://pbs.twimg.com/profile_images/556526218989027328/ERy-0VYQ.jpeg"},
+  # %Danton.User{name: "Kevin", status: "active", email: "kevinzussman@gmail.com", avatar: "https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAfOAAAAJDJiYTQwM2NhLWZlNjgtNDI2NS1hYTRjLTBkMzBmNjA3MTQxNg.jpg"},
 ]
 
 users |> Enum.each(&Repo.insert!/1)
@@ -41,7 +40,6 @@ auth_from_user = fn user ->
 	%Danton.Authorization{uid: user.email, provider: "identity", token: "$2b$12$bDyyBttsoYbkQSKMcoryeOtVilqpcFE2WDXsf2.s7lLFwLUoM6BpO", user_id: user.id}
 end
 
-# passwords are "222222222"
 Enum.map(made_users, auth_from_user) |> Enum.each(&Repo.insert!/1)
 
 # ==============
@@ -72,11 +70,11 @@ Enum.each(made_clubs, make_memberships)
 
 channels = [
 	%Danton.Channel{name: "Articles", description: "Good articles, opinion pieces, blog posts, etc"},
-	%Danton.Channel{name: "Sites", description:  "Interesting/Cool sites"},
+	%Danton.Channel{name: "Sites", description:  "Interesting/cool sites"},
 	%Danton.Channel{name: "Gear", description:  "Gear - clothing, gadgets, etc"},
   %Danton.Channel{name: "Videos", description:  "Videos or Gifs of any sort"},
 	%Danton.Channel{name: "Books", description: "Book recs"},
-	%Danton.Channel{name: "Music & Movies", description:  "Good albums, singles, bands, etc"},
+	%Danton.Channel{name: "Music & Movies", description:  "Good albums, singles, bands, movies, etc"},
   %Danton.Channel{name: "Misc", description:  "Things that don't fall into another category but are worth sharing."}
 ]
 
@@ -94,26 +92,23 @@ first_chan = hd made_chans
 # Make Posts
 # ==============
 
-posts = [
-	%Danton.Post{title: "Considerations on Cost Disease", description: "slate star post on costs going up", type: "link", url: "http://slatestarcodex.com/2017/02/09/considerations-on-cost-disease/"},
-	%Danton.Post{title: "The End of the Future", description: "Thiel's famous piece on technological stagnation", type: "link", url: "http://www.nationalreview.com/article/278758/end-future-peter-thiel"}
-]
+# posts = []
 
-Enum.each(posts, &(Danton.Channel.make_post_for_user(first_chan, first_user, &1)))
-made_posts = Repo.all(Danton.Post)
+# Enum.each(posts, &(Danton.Channel.make_post_for_user(first_chan, first_user, &1)))
+# made_posts = Repo.all(Danton.Post)
 
 # ==============
 # Make Rooms
 # ==============
 
-Enum.each(made_posts, &Danton.Post.make_room/1)
-first_room = Repo.get(Danton.Room, 1)
+# Enum.each(made_posts, &Danton.Post.make_room/1)
+# first_room = Repo.get(Danton.Room, 1)
 
 # ==============
 # Make Message
 # ==============
 
-Danton.Room.make_message(first_room, %{body: "Thought this was pretty interesting", user_id: first_user.id})
+# Danton.Room.make_message(first_room, %{body: "Thought this was pretty interesting", user_id: first_user.id})
 
 # ==============
 
