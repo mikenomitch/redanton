4e54a97531c6f12fe432a3d58a880b78b49d86dd
Michael Nomitch <mikenomitch@gmail.com>
2017-10-07 17:53:03 -0500

    post url links seem to work

--- web/views/post_view.ex
+++ web/views/post_view.ex
@@ -5,6 +5,20 @@ defmodule Danton.PostView do
   #   render(Danton.PostView, "stream.html", posts: posts)
   # end
 
+  # helpers for stream
+  def parse_link(post) do
+    if (post.url && String.length(post.url) > 0) do
+      has_scheme = String.contains? post.url, "http"
+      if (has_scheme) do
+        post.url
+      else
+        "http://#{post.url}"
+      end
+    else
+      "/posts/#{post.id}"
+    end
+  end
+
   # helpers for show
   def msg_class(message, current_user_id) do
     if (message.user_id == current_user_id) do
