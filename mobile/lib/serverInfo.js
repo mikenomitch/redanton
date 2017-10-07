c51d2407e4f8c803e398b841388c5630ff5b206e
Michael Nomitch <mikenomitch@gmail.com>
2017-10-07 11:33:06 -0500

    reverting db change for laptop

--- mobile/lib/serverInfo.js
+++ mobile/lib/serverInfo.js
@@ -1,4 +1,4 @@
-export const serverUrl = 'https://stormy-reef-53700.herokuapp.com'
-// export const serverUrl = 'https://e5445776.ngrok.io'
+// export const serverUrl = 'https://stormy-reef-53700.herokuapp.com'
+export const serverUrl = 'https://d339071b.ngrok.io'
 
 export default { serverUrl }
