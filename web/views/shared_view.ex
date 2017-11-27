c6149148324a3dc4a74cfba907fc67d9240ac6f5
Michael Nomitch <mikenomitch@gmail.com>
2017-11-26 19:58:31 -0600

    moved pager to partial

---
+++ web/views/shared_view.ex
@@ -0,0 +1,23 @@
+defmodule Danton.SharedView do
+  use Danton.Web, :view
+
+  def show_pager(total_pages) do
+    total_pages > 1
+  end
+
+  def show_prev(current_page) do
+    current_page > 1
+  end
+
+  def show_next(current_page, total_pages) do
+    current_page < total_pages
+  end
+
+  def prev_link(base_url, current_page) do
+    base_url <> "?page=" <> Integer.to_string (current_page - 1)
+  end
+
+  def next_link(base_url, current_page) do
+    base_url <> "?page=" <> Integer.to_string (current_page + 1)
+  end
+end
