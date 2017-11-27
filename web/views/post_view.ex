8557a9d3830f3f66f33612bf23b216e1c374987a
Michael Nomitch <mikenomitch@gmail.com>
2017-11-26 19:43:40 -0600

    pager on homepage

--- web/views/post_view.ex
+++ web/views/post_view.ex
@@ -23,6 +23,26 @@ defmodule Danton.PostView do
     end
   end
 
+  def show_pager(total_pages) do
+    total_pages > 1
+  end
+
+  def show_prev(current_page) do
+    current_page > 1
+  end
+
+  def show_next(current_page, total_pages) do
+    current_page < total_pages
+  end
+
+  def prev_link(current_page) do
+    "/front?page=" <> Integer.to_string (current_page - 1)
+  end
+
+  def next_link(current_page) do
+    "/front?page=" <> Integer.to_string (current_page + 1)
+  end
+
   # helpers for show
   def msg_class(message, current_user_id) do
     if (message.user_id == current_user_id) do
