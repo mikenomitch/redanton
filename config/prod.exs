486f86af12a522c5dbedf42b94fec370f256289c
Michael Nomitch <mikenomitch@gmail.com>
2017-12-06 18:41:58 -0600

    changing danton url to relayd at app level

--- config/prod.exs
+++ config/prod.exs
@@ -19,7 +19,7 @@ use Mix.Config
 
 config :danton, Danton.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [scheme: "https", host: "danton.herokuapp.com", port: 443],
+  url: [scheme: "https", host: "relayd.io", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
--- mobile/lib/serverInfo.js
+++ mobile/lib/serverInfo.js
@@ -1,4 +1,4 @@
-export const serverUrl = 'https://danton.herokuapp.com'
+export const serverUrl = 'https://relayd.io'
 // export const serverUrl = 'https://1536dc1d.ngrok.io'
 
 export default { serverUrl }
