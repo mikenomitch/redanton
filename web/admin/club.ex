bcd8dd4da734d363d4968630f4a85cea24c564c1
Michael Nomitch <mikenomitch@gmail.com>
2017-12-11 18:25:39 -0600

    added notification to admin

--- web/admin/club.ex
+++ web/admin/club.ex
@@ -2,6 +2,5 @@ defmodule Danton.ExAdmin.Club do
   use ExAdmin.Register
 
   register_resource Danton.Club do
-
   end
 end
---
+++ web/admin/notification.ex
@@ -0,0 +1,6 @@
+defmodule Danton.ExAdmin.Notification do
+  use ExAdmin.Register
+
+  register_resource Danton.Notification do
+  end
+end
